06ffc8ea6d7cfb373e6e587599e1767bf205d1b0
aartvanbochove <64839449+aartvanbochove@users.noreply.github.com>
2021-02-15 16:05:17 +0100

    Bugfixes

--- Code setup/main.py
+++ Code setup/main.py
@@ -1,12 +1,11 @@
-import os
-import config
-from single_slice import selectslice
 from transform import transform
 from visualize import visualize2d, visualize3d
 from register import register2d, register3d
 
-register3d(107,102,'parameters_samplespace_MR.txt')
-#selectslice(patientfixed,slicenr,data_path)
-#transform("107_0",3)
-#visualize2d(107,107,0,3)
-visualize3d(107,102,0,5)
+#register2d(107,102,0,'parameters_samplespace_MR_2D.txt')
+
+#Before this step: change offset in parameterfile from fixed to moving image!
+
+transform(102,7)
+visualize2d(107,102,0,7)
+#visualize3d(107,102,50,6)
