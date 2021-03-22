b5a8d7265b9a7aee5006629f9d7a77fdf0b9dd43
Lisanvh <55533809+Lisanvh@users.noreply.github.com>
2021-03-22 10:39:39 +0100

    Add files via upload

---
+++ Project 2 (Machine learning)/Augmentation Normalization Per Category/Cats_and_Dogs/Normalization_cats_and_dogs.py
@@ -0,0 +1,96 @@
+import os
+import imageio
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import cv2
+
+input_dir = "images/"
+target_dir = "annotations/trimaps/"
+
+
+#%% Normalize
+def normalize_img(img): 
+    # Enable when normalizing all values between 0 and 1:
+    #img=img/np.amax(img)
+    
+    # Enable when normalizing mean 0 std 1:
+    img = (img - np.mean(img))/np.std(img)
+    
+    # resizing images to 128 x 128
+    img = cv2.resize(img, (128,128), interpolation = cv2.INTER_CUBIC)
+    return img
+
+def normalize_mask(mask): 
+
+    # resizing masks to 128 x 128
+    mask = cv2.resize(mask, (128,128), interpolation = cv2.INTER_NEAREST)
+    max_value = np.amax(mask)
+    min_value = np.amin(mask)
+    for i in range(128):
+        for j in range(128):
+            if mask[i,j]==max_value:
+                mask[i,j]=1
+            if mask[i,j]==min_value:
+                mask[i,j]=1
+            else:
+                mask[i,j]=0
+
+    return mask
+
+#%% Making list of paths for all images and masks
+input_img_paths = sorted(
+    [
+        os.path.join(input_dir, fname)
+        for fname in os.listdir(input_dir)
+        if fname.endswith(".jpg")])
+target_img_paths = sorted(
+    [
+        os.path.join(target_dir, fname)
+        for fname in os.listdir(target_dir)
+        if fname.endswith(".png") and not fname.startswith(".")])
+
+#%% Some images gave errors (idk why), so I removed them manually
+input_img_paths.remove('images/Abyssinian_34.jpg')
+input_img_paths.remove('images/Egyptian_Mau_139.jpg')
+input_img_paths.remove('images/Egyptian_Mau_145.jpg')
+input_img_paths.remove('images/Egyptian_Mau_167.jpg')
+input_img_paths.remove('images/Egyptian_Mau_177.jpg')
+input_img_paths.remove('images/Egyptian_Mau_191.jpg')
+target_img_paths.remove('annotations/trimaps/Abyssinian_34.png')
+target_img_paths.remove('annotations/trimaps/Egyptian_Mau_139.png')
+target_img_paths.remove('annotations/trimaps/Egyptian_Mau_145.png')
+target_img_paths.remove('annotations/trimaps/Egyptian_Mau_167.png')
+target_img_paths.remove('annotations/trimaps/Egyptian_Mau_177.png')
+target_img_paths.remove('annotations/trimaps/Egyptian_Mau_191.png')
+
+#%% Put all images and masks in a list
+List_images = []
+List_masks = []
+runner = 0
+for i in range(100):
+    runner = runner+1
+    print(runner)
+    img = imageio.imread(input_img_paths[i])
+    mask = imageio.imread(target_img_paths[i])
+    
+    # some images are different shapes (:,:,3) and some only (:,:)
+    if len(img.shape)>2:
+        img = img[:,:,0]
+    
+    # removing images that are very big, since cropping them to 256x256 gives very small parts of cats/dogs
+    if img.shape[0]>128:
+        if img.shape[0]<512:
+            if img.shape[1]>128:
+                if img.shape[1]<512:
+                    img = normalize_img(img)
+                    mask = normalize_mask(mask)
+                    List_images.append(img)
+                    List_masks.append(mask)
+
+#%% Plot one random animal + mask
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].imshow(List_images[10],cmap='gray')
+ax[0].set_title('Image')
+ax[1].imshow(List_masks[10],cmap='gray')
+ax[1].set_title('Mask')
---
+++ Project 2 (Machine learning)/Augmentation Normalization Per Category/Heart_Data/Normalization_and_Augmentation_heart.py
@@ -0,0 +1,247 @@
+import torch
+import numpy as np
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import torchvision
+import random
+import difflib
+import scipy.spatial
+from glob import glob
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+import nibabel as nib
+import gryds
+import random
+import cv2
+
+#%%
+
+# Normalization
+
+def normalize_img(img): 
+    # Enable when normalizing all values between 0 and 1:
+    #img=img/np.amax(img)
+    
+    # Enable when normalizing mean 0 std 1:
+    img = (img - np.mean(img))/np.std(img)
+    img = cv2.resize(img, (128,128), interpolation = cv2.INTER_CUBIC)
+    return img
+
+def normalize_mask(mask): 
+    mask[mask>1]=1
+    # resizing masks to 128 x 128
+    mask = cv2.resize(mask, (128,128), interpolation = cv2.INTER_NEAREST)
+    return mask
+
+#%%
+    
+def Bspline(img, mask):
+    # Define a random 3x3 B-spline grid for a 2D image:
+    random_grid = np.random.rand(2, 3, 3)
+    random_grid -= 0.5
+    random_grid /= 5
+
+    # Define a B-spline transformation object
+    bspline = gryds.BSplineTransformation(random_grid)
+
+    # Define an interpolator object for the image:
+    interpolator_img = gryds.Interpolator(img)
+    interpolator_mask = gryds.Interpolator(mask)
+    
+    # Transform the image using the B-spline transformation
+    transformed_image = interpolator_img.transform(bspline)
+    transformed_mask = interpolator_mask.transform(bspline)
+
+    return transformed_image, transformed_mask
+
+def Affine(img,mask):
+    # Define a scaling transformation object
+    angle = random.randrange(-1,2,2)*np.pi/(random.randint(6,16))
+    center_point_x = 0.1*random.randint(3,7)
+    center_point_y = 0.1*random.randint(3,7)
+    affine = gryds.AffineTransformation(
+    ndim=2,
+    angles=[angle], # List of angles (for 3D transformations you need a list of 3 angles).
+    center=[center_point_x, center_point_y])  # Center of rotation.
+    
+    # Define an interpolator object for the image:
+    interpolator_img = gryds.Interpolator(img)
+    interpolator_mask = gryds.Interpolator(mask)
+    
+    # Transform image and mask using Affine transformation
+    transformed_image = interpolator_img.transform(affine)
+    transformed_mask = interpolator_mask.transform(affine)
+    return transformed_image, transformed_mask
+
+def flip(img, mask): # Check if it properly works
+    img = torch.from_numpy(img.copy())
+    mask = torch.from_numpy(mask.copy())
+    flipped_img = torchvision.transforms.functional.hflip(img = img) # change to .vflip for vertical flip
+    flipped_mask = torchvision.transforms.functional.hflip(img = mask)
+    flipped_img = flipped_img.cpu().detach().numpy()
+    flipped_mask = flipped_mask.cpu().detach().numpy()
+    return flipped_img, flipped_mask
+
+def Bspline_and_Affine(img, mask):
+    # Define a scaling transformation object
+    angle = random.randrange(-1,2,2)*np.pi/(random.randint(6,16))
+    center_point_x = 0.1*random.randint(3,7)
+    center_point_y = 0.1*random.randint(3,7)
+    affine = gryds.AffineTransformation(
+    ndim=2,
+    angles=[angle], # List of angles (for 3D transformations you need a list of 3 angles).
+    center=[center_point_x, center_point_y])  # Center of rotation.
+    
+    # Define a random 3x3 B-spline grid for a 2D image:
+    random_grid = np.random.rand(2, 3, 3)
+    random_grid -= 0.5
+    random_grid /= 5
+    
+    # Define a B-spline transformation object
+    bspline = gryds.BSplineTransformation(random_grid)
+    
+    # Define an interpolator object for the image:
+    interpolator_img = gryds.Interpolator(img)
+    interpolator_mask = gryds.Interpolator(mask)
+    
+    # Transform the image using both transformations. The B-spline is applied to the
+    # sampling grid first, and the affine transformation second. From the
+    # perspective of the image itself, the order will seem reversed (!).
+    transformed_image = interpolator_img.transform(bspline, affine)
+    transformed_mask = interpolator_mask.transform(bspline, affine)
+    return transformed_image, transformed_mask
+    
+def Bspline_and_Affine_flipped(img, mask):
+    # Define a scaling transformation object
+    angle = random.randrange(-1,2,2)*np.pi/(random.randint(6,16))
+    center_point_x = 0.1*random.randint(3,7)
+    center_point_y = 0.1*random.randint(3,7)
+    affine = gryds.AffineTransformation(
+    ndim=2,
+    angles=[angle], # List of angles (for 3D transformations you need a list of 3 angles).
+    center=[center_point_x, center_point_y])  # Center of rotation.
+    
+    # Define a random 3x3 B-spline grid for a 2D image:
+    random_grid = np.random.rand(2, 3, 3)
+    random_grid -= 0.5
+    random_grid /= 5
+    
+    # Define a B-spline transformation object
+    bspline = gryds.BSplineTransformation(random_grid)
+    
+    # Define an interpolator object for the image:
+    interpolator_img = gryds.Interpolator(img)
+    interpolator_mask = gryds.Interpolator(mask)
+    
+    # Transform the image using both transformations. The B-spline is applied to the
+    # sampling grid first, and the affine transformation second. From the
+    # perspective of the image itself, the order will seem reversed (!).
+    transformed_image = interpolator_img.transform(bspline, affine)
+    transformed_mask = interpolator_mask.transform(bspline, affine)
+    
+    img = torch.from_numpy(transformed_image.copy())
+    mask = torch.from_numpy(transformed_mask.copy())
+    flipped_img = torchvision.transforms.functional.hflip(img = img) # change to .vflip for vertical flip
+    flipped_mask = torchvision.transforms.functional.hflip(img = mask)
+    transformed_image = flipped_img.cpu().detach().numpy()
+    transformed_mask = flipped_mask.cpu().detach().numpy()
+    
+    return transformed_image, transformed_mask
+    
+#%%
+
+# Opening external dataset
+#data_path = (r'C:\Users\darja\Documents\TuE\elastix-5.0.1-win64\TrainingData\Task05_Prostate')
+data_path = r"C:\Users\s160518\Documents\CSMIA Segmentation\Task02_Heart\Task02_Heart"
+
+
+# number_list SHORT, this list should be used just to check the code for the first 3 patients
+#number_list = [3, 4, 5]
+
+# number_list COMPLETE, this list should be used in the end for all the patients
+number_list = [3, 4, 5, 7, 9, 10, 11, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 29, 30]
+
+# List of the numbers of slices per patient.   
+slice_list = [130, 110, 120, 130, 100, 120, 120, 120, 90, 120, 122, 100, 110, 100, 110, 110, 120, 120, 120, 110]
+
+i = -1
+List_images = []
+List_masks = []
+
+# loop the patients
+#for number in number_list:
+for number in number_list:   
+    i = i + 1
+    mask_path = os.path.join(data_path,f"labelsTr\la_{number}.nii.gz"); 
+    img_path  = os.path.join(data_path,f"imagesTr\la_{number}.nii.gz")
+    
+    List_img0 = slice_list[i] * [0]
+    List_img1 = slice_list[i] * [0]
+    List_img2 = slice_list[i] * [0]
+    List_img3 = slice_list[i] * [0]
+    List_img4 = slice_list[i] * [0]
+    List_img5 = slice_list[i] * [0]
+    
+    List_mask0 = slice_list[i] * [0]
+    List_mask1 = slice_list[i] * [0]
+    List_mask2 = slice_list[i] * [0]
+    List_mask3 = slice_list[i] * [0]
+    List_mask4 = slice_list[i] * [0]
+    List_mask5 = slice_list[i] * [0]
+    # Loop the slices 
+    #for slice in range(slice_list[i]):
+    for slice in range(slice_list[i]):
+        mask = np.rot90(nib.load(mask_path).get_data()[:,:,slice])
+        img  = np.rot90(nib.load(img_path).get_data()[:,:,slice]) 
+        print ('Patient',number,'slice',slice)
+        img = normalize_img(img)
+        mask = normalize_mask(mask)
+        
+        # Original image
+        List_img0[slice] = img
+        List_mask0[slice] = mask
+        
+        # Bspline and Affine and flipped transformation
+        img_bspline_affine_flipped, mask_bspline_affine_flipped = Bspline_and_Affine_flipped(img, mask)
+        
+        List_img1[slice] = img_bspline_affine_flipped
+        List_mask1[slice] = mask_bspline_affine_flipped
+        
+        # Bspline and Affine and flipped transformation second time
+        img_bspline_affine_flipped1, mask_bspline_affine_flipped1 = Bspline_and_Affine_flipped(img, mask)
+        
+        List_img2[slice] = img_bspline_affine_flipped1
+        List_mask2[slice] = mask_bspline_affine_flipped1
+        
+        # Bspline and Affine transformation
+        img_bspline_affine, mask_bspline_affine = Bspline_and_Affine(img, mask)
+        
+        List_img3[slice] = img_bspline_affine
+        List_mask3[slice] = mask_bspline_affine
+        
+        # Bspline and Affine transformation second time
+        img_bspline_affine1, mask_bspline_affine1 = Bspline_and_Affine(img, mask)
+        
+        List_img4[slice] = img_bspline_affine1
+        List_mask4[slice] = mask_bspline_affine1
+        
+        # Bspline transformation
+        img_bspline, mask_bspline = Bspline(img,mask)
+        
+        List_img5[slice] = img_bspline
+        List_mask5[slice] = mask_bspline
+        
+    List_images.extend([List_img0, List_img1, List_img2, List_img3, List_img4, List_img5])
+    List_masks.extend([List_img0, List_mask1, List_mask2, List_mask3, List_mask4, List_mask5])
+
+
+image_plot = List_images[0][1]
+mask_plot = List_masks[0][1]
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].imshow(image_plot, cmap='gray')
+ax[0].set_title('Image')
+ax[1].imshow(mask_plot,cmap='gray')
+ax[1].set_title('Mask')
+
+
